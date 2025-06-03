b1cb4854d7c46aa4515f7dbe87c95fe45161d69e
cosmic-bkyle <b.k.kyle01@gmail.com>
2025-06-03 14:03:44 -0400

    add regress function and initial result

--- regression.py
+++ regression.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import time
 from sklearn.linear_model import ElasticNetCV 
 import csv, pathlib, helpers, state
+import pandas as pd
 
 NUM_SCRAMBLES = 1000
 
@@ -44,13 +45,11 @@ def visualize(X):
     plt.ylabel("scramble index")
     plt.show()
 
-
-def main():
+def append_to_csv(filename):
     '''
-    compute data for the linear regression.
+    add (scramble, soln length) pairs to the specified csv file
     '''
-    scrambles = []
-    OUT = pathlib.Path("scrambles.csv")
+    OUT = pathlib.Path(filename)
     header_written = OUT.exists()      # don’t duplicate header
 
     with OUT.open("a", newline="") as f:
@@ -60,24 +59,20 @@ def main():
         for block in range(100): #because nissy is often giving seg faults, compute scrambles in blocks
             scramble_block = helpers.get_dr_scrambles(NUM_SCRAMBLES)
             soln_block = helpers.get_solns(scramble_block)
-            scrambles += scramble_block
-            writer.writerows(zip(scramble_block, soln_block))
+            writer.writerows(zip(scramble_block, soln_block)) #guarantees same lengths
             print(f"block {block:03d} done")
-            
+
+
+def regress(X, y):
+    '''
+    Apply a lasso model to learn the weights of the features.
+    X: 
+    
     '''
 
 
 
-    
-
-    
-    scrambles.pop()
-
-    bips = state.Cube.get_bips(scrambles)
-    X = make_design_matrix(bips)
-    y = np.array(soln_lengths)
-    print(len(scrambles))
-    print(len(soln_lengths))
+    start = time.time()
     model = ElasticNetCV(cv=5, l1_ratio=0.7).fit(X, y) #lasso model
     w  = model.coef_.reshape(8, 12)    # w[c, e]
     bias = model.intercept_
@@ -90,10 +85,28 @@ def main():
     plt.xlabel("Edge ID (0‒11)")
     plt.ylabel("Corner ID (0‒7)")
     plt.tight_layout()
+    print("this took "+ str(time.time() - start))
     plt.show()
 
-    #TODO: recompute the state.py functions specifically for the important domino pieces, with 8 corners and 8 edges.
+
+
+def main():
     '''
+    
+    '''
+    #append_to_csv("scrambles.csv") #generate scramble, solution pairs.
+    start = time.time()
+    df = pd.read_csv("scrambles.csv",
+                 names=["scramble", "opt_len"],   # give column names
+                 header=None)
+    scrambles = df["scramble"].tolist()
+    soln_lengths    = df["opt_len"].astype(int).tolist()
+    y = np.array(soln_lengths)
+    bips = state.Cube.get_bips(scrambles) # finishing this line takes 10.83s
+    X = make_design_matrix(bips) # finishing this line takes 100s.
+
+    regress(X, y)
+    
     
 
 if __name__ == "__main__":
