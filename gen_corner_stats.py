3dc6e4ed1456055251308d2260a5c69207007fb1
cosmic-bkyle <b.k.kyle01@gmail.com>
2025-06-16 15:42:31 -0400

    fix typo

--- gen_corner_stats.py
+++ gen_corner_stats.py
@@ -1,15 +1,8 @@
 import helpers
-import sys
-import random
-import subprocess
 import pandas as pd
 import time
-import re
-import math
-import pathlib
-import csv
 
-NUM_SCRAMBLES = 100
+NUM_SCRAMBLES = 1000
 
 def main():
     '''generate ~10k scrambles. Compute corner solution length and optimal solution length on all. 
@@ -43,34 +36,6 @@ def main():
     print("it took " + str(time.time() - start))
 
     return None
-
-
-
-    '''
-    
-    if not (len(scrambles) == len(corners_block) == len(solution_lengths)):
-        print("Error, some scrambles were not solved.")
-    
-    df = pd.DataFrame({
-        "scramble"      : scrambles,
-        "corner solution length"    : corners_lengths,
-        "average solution length"   : solution_lengths,
-    })
-
-    stats = (df
-             .groupby("corner solution length")["average solution length"]
-             .agg(["count", "mean", "std"])
-             .sort_index()
-             .round(3))
-
-    stats.to_csv("corner_stats_ben.csv", index_label="corner solution length ")
-    print("it took " + str(time.time() - start))
-
-    
-    '''
-        
-
-
 
 if __name__ == "__main__":
     main() 
